1c21b9216d87665713fa03fdc93b1308c5b1f0c7
timeshow <397975896@qq.com>
2023-08-09 11:17:17 +0800

    feat: RequestCriteria

---
+++ src/Criteria/RequestCriteria.php
@@ -0,0 +1,145 @@
+<?php
+declare(strict_types=1);
+namespace TimeShow\Repository\Criteria;
+
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Database\Query\Builder as DatabaseBuilder;
+use Illuminate\Http\Request;
+use TimeShow\Repository\Criteria\Common\OrderBy;
+use TimeShow\Repository\Interfaces\CriteriaInterface;
+
+class RequestCriteria extends AbstractCriteria
+{
+    protected int $page = 1;
+    protected int $size = 10;
+    /** @var array<RequestCriteriaColumn> */
+    protected array $columns = [];
+    protected int $total = 0;
+    protected bool $isPagination = true;
+    /** @var array<CriteriaInterface> */
+    protected array $globalCriteriaList = [];
+    protected array $relations = [];
+
+    public function __construct(protected Request $request)
+    {
+        if ($this->request->query->has('page')) {
+            $this->page = (int) $this->request->query('page');
+        }
+        if ($this->request->query->has('size')) {
+            $this->size = (int) $this->request->query('size');
+        }
+    }
+
+    public function disablePagination(): static
+    {
+        $this->isPagination = false;
+        return $this;
+    }
+
+    public function globalCriteria(CriteriaInterface $criteria): void
+    {
+        $this->globalCriteriaList[] = $criteria;
+    }
+
+    public function getPaginate(): array
+    {
+        return [
+            'size' => $this->size,
+            'page' => $this->page,
+            'total' => $this->total,
+        ];
+    }
+
+    public static function from(Request $request): static
+    {
+        return new RequestCriteria($request);
+    }
+
+    public function column($field): RequestCriteriaColumn
+    {
+        $column = new RequestCriteriaColumn($field);
+        $this->columns[$column->field] = $column;
+
+        return $column;
+    }
+
+    public function columns($defaultColumns = ['id', 'created_at', 'updated_at']): array
+    {
+        return [...array_keys($this->columns), ...$defaultColumns];
+    }
+
+    public function getSearchFieldVal(RequestCriteriaColumn $column, $default = '')
+    {
+        $searchField = $column->searchFieldName();
+        if ($this->request->query->has($searchField)) {
+            return (string) $this->request->query->get($searchField);
+        }
+
+        return $default;
+    }
+
+    public function hasQueryField(string $field): bool
+    {
+        return $this->request->query->has($field);
+    }
+
+    public function getQueryField($field): mixed
+    {
+        return $this->request->query->get($field);
+    }
+
+    public function hasSearchFieldVal(RequestCriteriaColumn $column): bool
+    {
+        return $this->request->query->has($column->searchFieldName());
+    }
+
+    /**
+     * @return array<CriteriaInterface>
+     */
+    public function getSearchCriteriaList(): array
+    {
+        $list = $this->globalCriteriaList;
+        foreach ($this->columns as $column) {
+            if ($searchField = $column->searchFieldName()) {
+                if (! $this->request->query->has($searchField)) {
+                    continue;
+                }
+                $fieldVal = $this->request->query($searchField);
+                $criteria = $column->matchCriteria($fieldVal);
+                if (! $criteria instanceof NullCriteria) {
+                    $list[] = $criteria;
+                }
+            }
+            if ($sortableField = $column->sortableFieldName()) {
+                $direction = $column->sortDirection;
+                if ($this->request->query->has($sortableField)) {
+                    $directionVal = $this->request->query($sortableField);
+                    if ($directionVal === 'asc' || $directionVal === 'desc') {
+                        $direction = $directionVal;
+                    }
+                }
+                $list[] = new OrderBy($column->field, $direction);
+            }
+        }
+
+        return $list;
+    }
+
+    protected function applyToQuery(Model|Relation|EloquentBuilder|DatabaseBuilder $model): Model|Relation|DatabaseBuilder|EloquentBuilder
+    {
+        foreach ($this->getSearchCriteriaList() as $criteria) {
+            $model = $criteria->apply($model, $this->repository);
+        }
+
+        if ($this->isPagination) {
+            $this->total = $model->count();
+
+            return $model->forPage($this->page, $this->size);
+        }
+
+        return $model;
+    }
+
+}
---
+++ src/Criteria/RequestCriteriaColumn.php
@@ -0,0 +1,113 @@
+<?php
+declare(strict_types=1);
+
+namespace TimeShow\Repository\Criteria;
+
+
+use TimeShow\Repository\Criteria\Common\FieldIsValue;
+use TimeShow\Repository\Criteria\Common\FieldLikeValue;
+use TimeShow\Repository\Criteria\Common\GreaterThan;
+use TimeShow\Repository\Criteria\Common\GreaterThanOrEqual;
+use TimeShow\Repository\Criteria\Common\LessThan;
+use TimeShow\Repository\Criteria\Common\LessThanOrEqual;
+use TimeShow\Repository\Criteria\Common\NotEqual;
+use TimeShow\Repository\Criteria\NullCriteria;
+use TimeShow\Repository\Interfaces\CriteriaInterface;
+
+class RequestCriteriaColumn
+{
+    const FIELD_ORDER_PREFIX = 'o_';
+    const FIELD_SEARCH_PREFIX = 'f_';
+    public bool $searchable = false;
+    public bool $searchPrefix = false;
+    public string $searchOption = 'like';
+    public bool $sortable = false;
+    public string $sortDirection = 'asc';
+    public CriteriaInterface $criteria;
+
+    public function __construct(public string $field)
+    {
+        $this->criteria = new NullCriteria();
+    }
+
+    public function searchByCriteria(CriteriaInterface $criteria): static
+    {
+        $this->criteria = $criteria;
+        $this->searchOption = 'custom';
+
+        return $this;
+    }
+
+    public function searchable(bool $searchable): static
+    {
+        $this->searchable = $searchable;
+
+        return $this;
+    }
+
+    public function search(string $option = 'like'): static
+    {
+        $this->searchable = true;
+        $this->searchOption = $option;
+
+        return $this;
+    }
+
+    public function sortByDesc(): static
+    {
+        $this->sortable = true;
+        $this->sortDirection = 'desc';
+
+        return $this;
+    }
+
+    public function sortByAsc(): static
+    {
+        $this->sortable = true;
+        $this->sortDirection = 'asc';
+
+        return $this;
+    }
+
+    public function searchFieldName(bool $searchPrefix): string
+    {
+        if (!$this->searchable) {
+            return '';
+        }
+
+        $this->searchPrefix = $searchPrefix;
+        if ($this->searchPrefix) {
+            return self::FIELD_SEARCH_PREFIX . $this->field;
+        }
+        return $this->field;
+
+    }
+
+    public function sortableFieldName(bool $searchPrefix): string
+    {
+        if (!$this->sortable) {
+            return '';
+        }
+
+        $this->searchPrefix = $searchPrefix;
+        if ($this->searchPrefix) {
+            return self::FIELD_ORDER_PREFIX . $this->field;
+        }
+        return $this->field;
+    }
+
+    public function matchCriteria($fieldVal): CriteriaInterface
+    {
+        return match ($this->searchOption) {
+            '=' => new FieldIsValue($this->field, $fieldVal),
+            '~=' => new NotEqual($this->field, $fieldVal),
+            '<' => new LessThan($this->field, $fieldVal),
+            '<=' => new LessThanOrEqual($this->field, $fieldVal),
+            '>' => new GreaterThan($this->field, $fieldVal),
+            '>=' => new GreaterThanOrEqual($this->field, $fieldVal),
+            'like' => new FieldLikeValue($this->field, $fieldVal),
+            'custom' => $this->criteria,
+            default => new NullCriteria()
+        };
+    }
+}
